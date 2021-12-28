29fbd4814392a2a312ef5c8e88e29af07638f4dd
pycapa <bycapus@gmail.com>
2021-12-28 13:43:43 -0500

    Review 12282021

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,30 @@
-document.addEventListener("DOMContentLoaded", function(){
+/* document.addEventListener("DOMContentLoaded", function () { */
+
+window.addEventListener('scroll', function () {
+
+      let icon_up = document.querySelector('.icon-up');
+      let menu_bar = document.getElementById('navbar-menu');
+
+      if (window.scrollY > 50) {
+            menu_bar.classList.add('fixed-top');
+            icon_up.style.display = 'block';
+      } else {
+            menu_bar.classList.remove('fixed-top');
+            icon_up.style.display = 'none';
+      }
+});
+/* }); */
 
 
-    
-
-    window.addEventListener('scroll', function() {
-
-        if (window.scrollY > 50) {
-          document.getElementById('navbar-menu').classList.add('fixed-top');
-          // add padding top to show content behind navbar
-          navbar_height = document.querySelector('.navbar').offsetHeight;
-          document.body.style.paddingTop = navbar_height + 'px';
-        } else {
-          document.getElementById('navbar-menu').classList.remove('fixed-top');
-           // remove padding top from body
-          document.body.style.paddingTop = '0';
-        } 
-    });
-  }); 
-
-
-function active_item(vhref){
-  const menuItem = document.querySelectorAll('a.nav-link');
-  for (let i=0; i<menuItem.length; i++){
-    if (menuItem[i].href === vhref){
-      console.log('la referencia es ' + vhref)
-      console.log(menuItem[i].classList);
-      menuItem[i].classList.add('active');
-    } else {
-        menuItem[i].classList.remove('active');
-    }
-  }
+function active_item(vhref) {
+      const menuItem = document.querySelectorAll('a.nav-link');
+      for (let i = 0; i < menuItem.length; i++) {
+            if (menuItem[i].href === vhref) {
+                  console.log('la referencia es ' + vhref)
+                  console.log(menuItem[i].classList);
+                  menuItem[i].classList.add('active');
+            } else {
+                  menuItem[i].classList.remove('active');
+            }
+      }
 };
