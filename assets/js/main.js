c68888c13800b4d841b3474f58cdf7ba85c73930
pycapa <bycapus@gmail.com>
2021-12-30 19:07:17 -0500

    Last version

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 /* document.addEventListener("DOMContentLoaded", function () { */
 
-
+/* 
 window.addEventListener('scroll', function () {
 
       let icon_up = document.querySelector('.icon-up');
@@ -12,11 +12,11 @@ window.addEventListener('scroll', function () {
       } else {
             icon_up.style.display = 'none';
       }
-});
+}); */
 
 
 /* }); */
-
+/* 
 function active_item(vhref) {
       const menuItem = document.querySelectorAll('a.nav-link');
       for (let i = 0; i < menuItem.length; i++) {
@@ -28,9 +28,43 @@ function active_item(vhref) {
                   menuItem[i].classList.remove('active');
             }
       }
-};
+}; */
 
 
 function scroll_up(){
       document.scrollY = 0;
 }
+
+'use strict'
+// variables
+let navbar = document.getElementById('navbar');
+let button = document.getElementById("toggle-button");
+let menu = document.getElementById("navbarNav");
+let checked = document.getElementById('toggle-button');
+let label = document.getElementById('label-toggle-button');
+let icon = document.getElementById('icon-menu');
+
+button.addEventListener("click", () =>{
+    if(checked.checked){
+        menu.classList.add('show-menu');
+        label.classList.add('show-checked-button');
+        navbar.classList.add('menu-responsive');
+        icon.classList.remove('fa-ellipsis-v');
+        icon.classList.add('fa-times');
+        
+    }else {
+        menu.classList.remove('show-menu');
+        label.classList.remove('show-checked-button');
+        navbar.classList.remove('menu-responsive');
+        icon.classList.remove('fa-times');
+        icon.classList.add('fa-ellipsis-v');
+    };    
+}); 
+
+
+window.addEventListener("resize", ()=>{
+    let window_width = window.innerWidth;
+    if (window_width>=700 && checked.checked){
+        checked.click();
+    }
+})
